76db95e32e934cb28e2b8255f2268c284b4c1917
Suvankar <suvankarpaul004@gmail.com>
2025-11-30 22:24:33 +0530

    data ingestion done

--- demo.py
+++ demo.py
@@ -1,10 +1,6 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-# logging.info("welcome to our custom logger module")
 
-try:
-    a= 1 / 0
-except Exception as e:
-    raise USvisaException(e, sys)
+pipline=TrainPipeline()
+pipline.run_pipeline()
+
---
+++ us_visa/entity/config_entity.py
@@ -0,0 +1,60 @@
+import os
+from us_visa.constants import *
+from dataclasses import dataclass
+from datetime import datetime
+import logging
+
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+
+@dataclass
+class TrainingPipelineConfig:
+    pipeline_name: str = PIPELINE_NAME
+    artifact_dir: str = os.path.join(ARTIFACTS_DIR, TIMESTAMP)
+    timestamp: str = TIMESTAMP
+
+
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
+
+
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)
+    logging.info("Data Ingestion Config Created")
+    logging.info(f"Data Ingestion Directory: {data_ingestion_dir}")
+
+    feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
+    logging.info(f"Feature Store Path: {feature_store_file_path}")
+
+    training_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+    logging.info(f"Training File Path: {training_file_path}")
+
+    testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+    logging.info(f"Testing File Path: {testing_file_path}")
+
+    train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+    logging.info(f"Train-Test Split Ratio: {train_test_split_ratio}")
+
+    collection_name:str = DATA_INGESTION_COLLECTION_NAME
+    logging.info(f"MongoDB Collection Name: {collection_name}")
+
+
+
+@dataclass
+class DataValidationConfig:
+    data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
+    drift_report_file_path: str = os.path.join(data_validation_dir, DATA_VALIDATION_DRIFT_REPORT_DIR,
+                                               DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
+
+
+
+
+
+
+
+
+
+    
+
+
